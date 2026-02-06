9dce0ae3c1ecdf7557ddd68ed3197dce5070875c
TopMob <zak.shtorm6430@gmail.com>
2026-02-06 23:19:12 +0300

    init

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,12 +3,14 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyCtM3kS2F7P7m21Phx4QJenLIPbtgedRRw",
+  authDomain: "smartnotes-f5733.firebaseapp.com",
+  projectId: "smartnotes-f5733",
+  storageBucket: "smartnotes-f5733.firebasestorage.app",
+  messagingSenderId: "523799066979",
+  appId: "1:523799066979:web:abc13814f34864230cbb56",
+  clientId: "523799066979-e75bl0vvthlr5193qee8niocvkoqaknq.apps.googleusercontent.com"
+
 }
 
 const app = initializeApp(firebaseConfig)
